261f5975fd0cdc123bbaf85e5bca0424956dc113
caballo <imanol.l.p@hotmail.com>
2021-07-28 21:12:22 +0200

    Made and tested calculator

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,21 +1,17 @@
 const calculator = require("../code/calculator");
 
 test('sums 2 numbers', () => {
-    expect(calculator('sum', 2, 6)).toBe(8)
+    expect(calculator.sum(2, 6)).toBe(8)
 });
 
 test('substracts 2 numbers', () => {
-    expect(calculator('mul', 5, 5)).toBe(25)
+    expect(calculator.sub(5, 5)).toBe(0)
 })
 
 test('multiplies 2 numbers', () => {
-    expect(calculator('mul', 5, 5)).toBe(25)
+    expect(calculator.mul(5, 5)).toBe(25)
 })
 
 test('divides 2 numbers', () => {
-    expect(calculator('div', 10, 3)).toBeCloseTo(3.3)
+    expect(calculator.div(10, 3)).toBeCloseTo(3.3, 2)
 });
-
-test('does complex operations', () => {
-    expect(calculator('mul', 5, 5)).toBe(25)
-})
